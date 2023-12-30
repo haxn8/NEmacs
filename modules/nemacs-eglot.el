7d6dc41fd4e88ba9dff5583561bc437daae4c594
NateNateNate <natenatenat3@protonmail.com>
2023-12-30 13:15:16 +0800

    Eglot for PHP and JS working

--- modules/nemacs-eglot.el
+++ modules/nemacs-eglot.el
@@ -8,17 +8,21 @@
 (use-package eglot
   :ensure t
   :config
-  ;; Set up eglot for C/C++ mode with clangd
+  ;; Set up eglot for various modes
   (setq eglot-server-programs
 	`((c-mode . ("clangd"))
           (c++-mode . ("clangd"))
-          (js-mode . ("typescript-language-server" "--stdio" ,(concat "--tsserver-path=" ts-server-path)))))
+          (js-mode . ("typescript-language-server" "--stdio" ,(concat "--tsserver-path=" ts-server-path)))
+          (php-mode . ("intelephense" "--stdio"))))  ;; Intelephense for PHP mode
 
   ;; Automatically start eglot for C/C++ files
   (add-hook 'c-mode-hook 'eglot-ensure)
   (add-hook 'c++-mode-hook 'eglot-ensure)
 
   ;; Automatically start eglot for JavaScript files
-  (add-hook 'js-mode-hook 'eglot-ensure))
+  (add-hook 'js-mode-hook 'eglot-ensure)
+
+  ;; Automatically start eglot for PHP files
+  (add-hook 'php-mode-hook 'eglot-ensure))
 
 ;;; nemacs-eglot.el ends here
