b917b20cb3984050bb5956cbd9d36ac658ed0381
NateNateNate <natenatenat3@protonmail.com>
2023-12-30 13:57:15 +0800

    eglot pylsp

--- modules/nemacs-eglot.el
+++ modules/nemacs-eglot.el
@@ -13,7 +13,8 @@
 	`((c-mode . ("clangd"))
           (c++-mode . ("clangd"))
           (js-mode . ("typescript-language-server" "--stdio" ,(concat "--tsserver-path=" ts-server-path)))
-          (php-mode . ("intelephense" "--stdio"))))  ;; Intelephense for PHP mode
+          (php-mode . ("intelephense" "--stdio"))
+          (python-mode . ("pylsp"))))  ;; Use pylsp for Python mode
 
   ;; Automatically start eglot for C/C++ files
   (add-hook 'c-mode-hook 'eglot-ensure)
@@ -23,6 +24,9 @@
   (add-hook 'js-mode-hook 'eglot-ensure)
 
   ;; Automatically start eglot for PHP files
-  (add-hook 'php-mode-hook 'eglot-ensure))
+  (add-hook 'php-mode-hook 'eglot-ensure)
+
+  ;; Automatically start eglot for Python files
+  (add-hook 'python-mode-hook 'eglot-ensure))
 
 ;;; nemacs-eglot.el ends here
